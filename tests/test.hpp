8b183b1069147e0f93cb1a95473b4eefcb6b97eb
Neimheadh <contact@neimheadh.fr>
2019-12-30 15:50:29 +0100

    Harmonize test.hpp colors string

--- tests/test.hpp
+++ tests/test.hpp
@@ -3,7 +3,7 @@
 
 const char *C_GRAY = "\033[1;90m";
 const char *C_INFO = "\033[1;36m";
-const char *C_INFO2 = "\33[1;35m";
+const char *C_INFO2 = "\033[1;35m";
 const char *C_END = "\033[1;0m";
 const char *C_ERR = "\033[1;31m";
 
